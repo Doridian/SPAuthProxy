5bf59a32e99e461b8b5c9a629717933943d77f73
Doridian Draconia <mriq91@gmail.com>
2016-01-10 19:21:14 +0100

    guh

--- speedport.js
+++ speedport.js
@@ -177,7 +177,7 @@ Speedport.prototype.request = function (options, data, cb) {
 
 	var req = http.request(options, function (res) {
 		if (res.statusCode == 302 && res.headers.location.indexOf('/html/login/index.html') > 0) {
-			console.log('EERK', options.path, res.headers);
+			console.log('F', options.path, res.headers);
 			return self.login(function (err) {
 				if (err) {
 					return cb(err);
@@ -185,6 +185,7 @@ Speedport.prototype.request = function (options, data, cb) {
 				return self.request(options, data, cb);
 			});
 		}
+		console.log('S', options.path, res.headers);
 		if (self.cookieHeaders) {
 			res.headers['set-cookie'] = self.cookieHeaders;
 		}
