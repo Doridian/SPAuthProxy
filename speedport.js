7c6586876c585a3874b83721b8d40923b61c5189
Doridian Draconia <mriq91@gmail.com>
2016-01-10 19:13:24 +0100

    wat

--- speedport.js
+++ speedport.js
@@ -58,7 +58,7 @@ Speedport.prototype._dataRequest = function (options, data, cb) {
 		return cb(null, req);
 	});
 	req.on('error', function(err) {
-		console.error('DRE', err);
+		console.error('DRE', options.path, err);
 		cb(err);
 	});
 	req.setTimeout(10000);
