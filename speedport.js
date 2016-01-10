d021e62624db9b7af1251954511835f403c4b765
Doridian Draconia <mriq91@gmail.com>
2016-01-10 19:18:54 +0100

    meh

--- speedport.js
+++ speedport.js
@@ -177,7 +177,7 @@ Speedport.prototype.request = function (options, data, cb) {
 
 	var req = http.request(options, function (res) {
 		if (res.statusCode == 302 && res.headers.location.indexOf('/html/login/index.html') > 0) {
-			console.log('EERK');
+			console.log('EERK', options.path);
 			return self.login(function (err) {
 				if (err) {
 					return cb(err);
