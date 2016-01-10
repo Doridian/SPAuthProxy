0a47855d6326a5c20e1f6ab59b4baa26bb8b16c2
Doridian Draconia <mriq91@gmail.com>
2016-01-10 19:33:09 +0100

    drain all

--- speedport.js
+++ speedport.js
@@ -177,6 +177,7 @@ Speedport.prototype.request = function (options, data, cb) {
 
 	var req = http.request(options, function (res) {
 		if (res.statusCode == 302 && res.headers.location.indexOf('/html/login/index.html') > 0) {
+			_httpDummyCB(res);
 			if (options.noLogin) {
 				return cb('Not logged in or 404');
 			}
