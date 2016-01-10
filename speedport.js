f41d60441f2626d661176bf203e2bcd538de5628
Doridian Draconia <mriq91@gmail.com>
2016-01-10 19:06:31 +0100

    bah

--- speedport.js
+++ speedport.js
@@ -5,8 +5,8 @@ var http = require('http');
 var JSON5 = require('json5');
 var pbkdf2 = require('pbkdf2');
 
-http.globalAgent.keepAlive = true;
-http.globalAgent.maxSockets = 3;
+//http.globalAgent.keepAlive = true;
+//http.globalAgent.maxSockets = 3;
 
 function _httpDummyCB(res) {
 	res.on('data', function () { });
